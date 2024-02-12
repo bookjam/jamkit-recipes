b3ec9facd7b6fbd86a44811ec81597bc1c33b758
Hanyeol Cho <hanyeol.cho@gmail.com>
2024-02-12 14:56:47 +0900

    Add a recipe: sbml-dynamic-loading

---
+++ sbml-dynamic-loading/catalogs/MainApp/catalog_main.js
@@ -0,0 +1,22 @@
+function on_loaded() {
+    if ($data["loaded"] !== "yes") {
+        Promise.all([
+            [ "Hanyeol",  "1st" ],
+            [ "Hyunjung", "2nd" ],
+            [ "Hakyong",  "3rd" ],
+            [ "Haeun",    "4th" ],
+        ].map(([ name, nth ]) => {
+            return read("catalog", "template.sbml", {
+                "name": name,
+                "nth": nth
+            });
+        }))
+            .then((messages) => {
+                view.data("display-unit", {
+                    "messages": messages.join("\n"),
+                    "loaded": "yes"
+                });
+                view.action("reload");        
+            });
+    }
+}
