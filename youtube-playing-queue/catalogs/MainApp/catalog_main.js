1102abee8180a2161d43597c28ada0c987bf5ee5
Hanyeol Cho <hanyeol.cho@gmail.com>
2024-02-24 16:29:05 +0900

    Add prev command

--- youtube-playing-queue/catalogs/MainApp/catalog_main.js
+++ youtube-playing-queue/catalogs/MainApp/catalog_main.js
@@ -5,64 +5,49 @@ const _video_playlist = [
     "dJdqn5v4Dkw",
     "ArmDp-zijuc"
 ];
-var _next_video_index = 0;
-var _current_player_number = 1;
+var _current_video_index = 0;
 
 function on_loaded() {
     _play_video_at_index(0);
 }
 
 function on_video_finished() {
-    _play_next_video();
+    _play_video_at_index(_get_next_video_index());
 }
 
 function next() {
-    _play_next_video();
+    _play_video_at_index(_get_next_video_index());
+}
+
+function prev() {
+    _play_video_at_index(_get_prev_video_index());
 }
 
 function _play_video_at_index(index) {
-    const current_player_number = _current_player_number;
-    const next_player_number = _current_player_number % 2 + 1;
-
-    _next_video_index = index;
-
-    view.object(`youtube.${current_player_number}`).property({ 
-        "video-id": _get_next_video_id(),
-        "hidden": "no"
+    _current_video_index = index;
+    
+    view.object("youtube").property({ 
+        "video-id": _video_playlist[_current_video_index]
     });
-    view.object(`youtube.${current_player_number}`).action("play");
-
-    view.object(`youtube.${next_player_number}`).property({ 
-        "video-id": _get_next_video_id(),
-        "hidden": "yes"
-    });
+    view.object("youtube").action("play");
 }
 
-function _play_next_video() {
-    const current_player_number = _current_player_number;
-    const next_player_number = _current_player_number % 2 + 1;
+function _get_next_video_index() {
+    var next_video_index = _current_video_index + 1;
 
-    view.object(`youtube.${next_player_number}`).action("play");
-    view.object(`youtube.${next_player_number}`).property({ 
-        "hidden": "no"
-    });
-
-    view.object(`youtube.${current_player_number}`).property({ 
-        "video-id": _get_next_video_id(),
-        "hidden": "yes"
-    });
-
-    _current_player_number = next_player_number;
-}
-
-function _get_next_video_id() {
-    const next_video_id = _video_playlist[_next_video_index];
-
-    _next_video_index = _next_video_index + 1;
-
-    if (_next_video_index >= _video_playlist.length) {
-        _next_video_index = 0;
+    if (next_video_index >= _video_playlist.length) {
+        next_video_index = 0;
     }
 
-    return next_video_id;
+    return next_video_index;
+}
+
+function _get_prev_video_index() {
+    var prev_video_index = _current_video_index - 1;
+
+    if (prev_video_index < 0) {
+        prev_video_index = _video_playlist.length - 1;
+    }
+
+    return prev_video_index;
 }
