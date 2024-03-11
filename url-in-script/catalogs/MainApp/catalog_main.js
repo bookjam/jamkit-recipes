656f2922260f7822a317f60b82164d6b3e1e6297
Hanyeol Cho <hanyeol.cho@gmail.com>
2024-03-11 16:01:08 +0900

    Add a recipe: url-in-script

--- url-in-script/catalogs/MainApp/catalog_main.js
+++ url-in-script/catalogs/MainApp/catalog_main.js
@@ -4,9 +4,9 @@ function parse_url(params) {
     view.object("sbml.info").action("load", {
         "filename": "url_info.sbml",
         "scheme": url.scheme,
-        "host": url.host,
-        "path": url.path,
-        "query": url.query,
+        "host": url.host || "N/A",
+        "path": url.path || "N/A",
+        "query": url.query || "N/A",
         "string": url.string() // This is not a propery
     });
 }
